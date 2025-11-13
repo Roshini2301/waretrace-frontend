e68ee472af6e3818605dbfd758e59c96657dc1ab
Roshini2301 <roshini.m2023ece@sece.ac.in>
2025-11-13 17:48:40 +0530

    Fix Vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// Change the "base" to your repo name when deploying to GitHub Pages
+// Example: https://username.github.io/repo-name/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: "waretrace-frontend", 
+});
